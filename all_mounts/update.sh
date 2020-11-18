471fbc5f9328adc666aa10e742b795e0db98d50b
cfuchs <cfuchs@patchcode.ch>
2020-11-18 19:52:51 +0100

    solid git master

--- all_mounts/update.sh
+++ all_mounts/update.sh
@@ -4,11 +4,27 @@
 test -z "$AGENT_USER" && exit -1
 test -z "$TARGET_HOST" && exit -1
 
+# parameters
 RESULT_FILE="$TARGET_HOST.txt"
+
+#
+# Prepare version control
+#
+
+git fetch master
+git checkout master
+
+#
+#  Execute
+#
 
 cd "$(dirname "$0")"
 test -f "$RESULT_FILE" && rm "$RESULT_FILE"
 ssh -n "${AGENT_USER}@${TARGET_HOST}" mount > "$RESULT_FILE"
+
+#
+#  Commit and Push
+#
 
 git add "$RESULT_FILE"
 git commit -a -m "$(basename "$0"), $TARGET_HOST, $(date)"
