67cc485e331d610d6462765fa1995c8fe3e1e50f
cfuchs <cfuchs@patchcode.ch>
2020-11-18 19:47:20 +0100

    write data to *.txt

--- all_mounts/update.sh
+++ all_mounts/update.sh
@@ -4,11 +4,13 @@
 test -z "$AGENT_USER" && exit -1
 test -z "$TARGET_HOST" && exit -1
 
-cd "$(dirname "$0")"
-test -f "$TARGET_HOST" && rm "$TARGET_HOST"
-ssh -n "${AGENT_USER}@${TARGET_HOST}" mount > "$TARGET_HOST"
+RESULT_FILE="$TARGET_HOST.txt"
 
-git add "$TARGET_HOST"
+cd "$(dirname "$0")"
+test -f "$RESULT_FILE" && rm "$RESULT_FILE"
+ssh -n "${AGENT_USER}@${TARGET_HOST}" mount > "$RESULT_FILE"
+
+git add "$RESULT_FILE"
 git commit -a -m "$(basename "$0"), $TARGET_HOST, $(date)"
 git pull --rebase=true
 git push
